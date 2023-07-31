9ff6c376d6bb3ee47eb56ba0c6ce00ae02a6e073
SeongWooHong <pluto0303@naver.com>
2023-07-31 17:18:05 +0900

    add flutter

---
+++ untitled/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+// 첫 페이지
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First'),
+      ),
+      body: ElevatedButton(
+        child:Text('다음 페이지로'),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second'),
+      ),
+      body: ElevatedButton(
+        child:Text('이전 페이지로'),
+        onPressed: (){},
+      ),
+    );
+  }
+}
+
+/*
+class _MyHomePageState extends State<MyHomePage> {
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제목'),
+      ),
+      body: Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.all(8.0),
+      )
+    );
+  }
+}*/
